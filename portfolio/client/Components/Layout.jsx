953fc08ed93973423d0a61961751fdc692c889b7
T. Tran <ttran375@my.centennialcollege.ca>
2024-05-22 18:56:25 +0000

    Wed, May 22, 2024, 6:56 PM +00:00

--- portfolio/client/Components/Layout.jsx
+++ portfolio/client/Components/Layout.jsx
@@ -2,12 +2,27 @@ import { Link } from "react-router-dom";
 export default function Layout() {
   return (
     <>
-      <h1>My Portfolio</h1>
-      <nav>
-        <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
-        <Link to="/education">Education</Link>|{" "}
-        <Link to="/project">Project</Link>| <Link to="/contact">Contact</Link>
-      </nav>
+      <div
+        style={{
+          position: "fixed",
+          display: "flex",
+          justifyContent: "center",
+          gap: "2rem",
+          background: "rgba(255,255,255,0.75)",
+          padding: "1rem",
+          top: 0,
+          width: "100%",
+          zIndex: 10,
+        }}
+      >
+        <h1>My Portfolio</h1>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/about">About</Link> |{" "}
+          <Link to="/education">Education</Link>|{" "}
+          <Link to="/project">Project</Link>| <Link to="/contact">Contact</Link>
+        </nav>
+      </div>
+
       <br />
       <hr />
     </>
